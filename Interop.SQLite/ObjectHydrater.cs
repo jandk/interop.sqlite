08e5aa04381877415b23cccd56792117b6be09b5
Jan De Kock <tjoener@gmail.com>
2014-06-12 21:39:38 +0200

    Split up SQLite3Enumerator into ObjectHydrater

---
+++ Interop.SQLite/ObjectHydrater.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Interop.SQLite
+{
+    internal delegate void SetterMethod<in T>(T value, IntPtr statementHandle, int columnIndex);
+
+    internal interface IObjectHydrater<in T>
+        where T : new()
+    {
+        void Hydrate(T value, IntPtr statementHandle);
+    }
+
+    internal abstract class ObjectHydrater<T>
+        : IObjectHydrater<T> where T : new()
+    {
+        protected static readonly List<PropertyInfo> WritableProperties;
+        protected static readonly Dictionary<string, SetterMethod<T>> SetterMethods;
+
+        protected static readonly MethodInfo[] ColumnGetters = new[]
+        {
+            null, // 0 --
+            typeof(NativeMethods).GetMethod("ColumnInt64"), // 1 SQLite3ColumnType.Integer
+            typeof(NativeMethods).GetMethod("ColumnDouble"), // 2 SQLite3ColumnType.Float
+            typeof(SQLite3Helper).GetMethod("ColumnTextPtr"), // 3 SQLite3ColumnType.Text
+            typeof(SQLite3Helper).GetMethod("ColumnBlobPtr"), // 4 SQLite3ColumnType.Blob
+            null // 5 SQLite3ColumnType.Null
+        };
+
+
+        static ObjectHydrater()
+        {
+            WritableProperties = typeof(T).GetProperties().Where(property => property.CanWrite).ToList();
+            var writablePropertyNames = WritableProperties.Select(pi => pi.Name.ToLowerInvariant());
+
+        }
+
+        protected ObjectHydrater()
+        {
+            if (SetterMethods != null)
+                return;
+
+            foreach (PropertyInfo writableProperty in WritableProperties)
+                SetterMethods.Add(writableProperty.Name, GenerateSetterMethod(writableProperty));
+        }
+
+        public void Hydrate(T value, IntPtr statementHandle)
+        {
+            foreach (var method in SetterMethods.Values)
+                method(value, statementHandle, i);
+        }
+
+        public abstract SetterMethod<T> GenerateSetterMethod(PropertyInfo propertyInfo);
+    }
+
+    internal class ReflectionHydrater<T>
+        : ObjectHydrater<T> where T : new()
+    {
+        public override SetterMethod<T> GenerateSetterMethod(PropertyInfo propertyInfo)
+        {
+            Type propertyType = propertyInfo.PropertyType;
+            TypeCode propertyTypeCode = Type.GetTypeCode(propertyType);
+            MethodInfo columnGetter = ColumnGetters[(int)SQLite3Convert.TypeCodeAffinities[(int)propertyTypeCode]];
+
+            return (v, stmt, idx) =>
+            {
+                object result = columnGetter.Invoke(null, new object[] { stmt, idx });
+                object changedType = Convert.ChangeType(result, propertyType);
+                propertyInfo.SetValue(v, changedType, null);
+            };
+        }
+    }
+
+    internal class MethodGeneratorHydrater<T>
+        : ObjectHydrater<T> where T : new()
+    {
+        internal static readonly OpCode[] CastOpCodeAffinities = new[]
+        {
+            OpCodes.Nop, //  0 TypeCode.Empty
+            OpCodes.Nop, //  1 TypeCode.Object
+            OpCodes.Nop, //  2 TypeCode.DbNull
+            OpCodes.Nop, //  3 TypeCode.Boolean
+            OpCodes.Conv_U2, //  4 TypeCode.Char
+            OpCodes.Conv_I1, //  5 TypeCode.SByte
+            OpCodes.Conv_U1, //  6 TypeCode.Byte
+            OpCodes.Conv_I2, //  7 TypeCode.Int16
+            OpCodes.Conv_U2, //  8 TypeCode.UInt16
+            OpCodes.Conv_I4, //  9 TypeCode.Int32
+            OpCodes.Conv_U4, // 10 TypeCode.UInt32
+            OpCodes.Nop, // 11 TypeCode.Int64
+            OpCodes.Conv_U8, // 12 TypeCode.UInt64
+            OpCodes.Conv_R4, // 13 TypeCode.Single
+            OpCodes.Nop, // 14 TypeCode.Double
+            OpCodes.Nop, // 15 TypeCode.Decimal
+            OpCodes.Nop, // 16 TypeCode.DateTime
+            OpCodes.Nop, // 17 --
+            OpCodes.Nop // 18 TypeCode.String
+        };
+
+        public override SetterMethod<T> GenerateSetterMethod(PropertyInfo propertyInfo)
+        {
+            var setter = new DynamicMethod("_set" + propertyInfo.Name, null, new[] { typeof(T), typeof(IntPtr), typeof(int) }, typeof(T), true);
+
+            ILGenerator generator = setter.GetILGenerator();
+            generator.Emit(OpCodes.Ldarg_0);
+            generator.Emit(OpCodes.Ldarg_1);
+            generator.Emit(OpCodes.Ldarg_2);
+            EmitColumnMethod(generator, propertyInfo.PropertyType);
+            EmitCastOpCode(generator, propertyInfo.PropertyType);
+            generator.Emit(OpCodes.Call, typeof(T).GetProperty(propertyInfo.Name).GetSetMethod());
+            generator.Emit(OpCodes.Ret);
+
+            return (SetterMethod<T>)setter.CreateDelegate(typeof(SetterMethod<T>));
+        }
+
+        private static void EmitColumnMethod(ILGenerator generator, Type type)
+        {
+            TypeCode typeCode = Type.GetTypeCode(type);
+
+            // If we have an object, check for an array
+            if (typeCode == TypeCode.Object)
+            {
+                if (type != typeof(byte[]))
+                    throw new SQLite3Exception("Invalid property type: " + type.Name);
+
+                generator.Emit(OpCodes.Call, ColumnGetters[(int)SQLite3ColumnType.Blob]);
+                return;
+            }
+
+            // TODO: Support for Decimal and DateTime and Nullable<T>
+            if (typeCode == TypeCode.Decimal || typeCode == TypeCode.DateTime)
+                throw new SQLite3Exception("Invalid property type: " + type.Name);
+
+            generator.Emit(OpCodes.Call, ColumnGetters[(int)SQLite3Convert.TypeCodeAffinities[(int)typeCode]]);
+        }
+
+        private static void EmitCastOpCode(ILGenerator generator, Type type)
+        {
+            if (type == typeof(byte[]))
+            {
+                generator.Emit(OpCodes.Unbox_Any, typeof(byte[]));
+                return;
+            }
+
+            TypeCode typeCode = Type.GetTypeCode(type);
+            OpCode opCode = CastOpCodeAffinities[(int)typeCode];
+
+            if (opCode != OpCodes.Nop)
+                generator.Emit(opCode);
+        }
+    }
+}
--- Interop.SQLite/SQLite3Enumerator.cs
+++ Interop.SQLite/SQLite3Enumerator.cs
@@ -1,155 +1,42 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Reflection;
-using System.Reflection.Emit;
+using System.Linq;
 
 namespace Interop.SQLite
 {
-    internal delegate void SetterMethod<in T>(T value, IntPtr statementHandle, int columnIndex);
-
     internal class SQLite3Enumerator<T>
         : IEnumerator<T>
         where T : new()
     {
-        #region Static
-
-        private static readonly MethodInfo[] ColumnGetters = new[]
-        {
-            null, // 0 --
-            typeof(NativeMethods).GetMethod("ColumnInt64"), // 1 SQLite3ColumnType.Integer
-            typeof(NativeMethods).GetMethod("ColumnDouble"), // 2 SQLite3ColumnType.Float
-            typeof(SQLite3Helper).GetMethod("ColumnTextPtr"), // 3 SQLite3ColumnType.Text
-            typeof(SQLite3Helper).GetMethod("ColumnBlobPtr"), // 4 SQLite3ColumnType.Blob
-            null // 5 SQLite3ColumnType.Null
-        };
-
-        private static readonly Dictionary<string, SetterMethod<T>> SetterMethods;
-
-        internal static readonly OpCode[] CastOpCodeAffinities = new[]
-        {
-            OpCodes.Nop, //  0 TypeCode.Empty
-            OpCodes.Nop, //  1 TypeCode.Object
-            OpCodes.Nop, //  2 TypeCode.DbNull
-            OpCodes.Nop, //  3 TypeCode.Boolean
-            OpCodes.Conv_U2, //  4 TypeCode.Char
-            OpCodes.Conv_I1, //  5 TypeCode.SByte
-            OpCodes.Conv_U1, //  6 TypeCode.Byte
-            OpCodes.Conv_I2, //  7 TypeCode.Int16
-            OpCodes.Conv_U2, //  8 TypeCode.UInt16
-            OpCodes.Conv_I4, //  9 TypeCode.Int32
-            OpCodes.Conv_U4, // 10 TypeCode.UInt32
-            OpCodes.Nop, // 11 TypeCode.Int64
-            OpCodes.Conv_U8, // 12 TypeCode.UInt64
-            OpCodes.Conv_R4, // 13 TypeCode.Single
-            OpCodes.Nop, // 14 TypeCode.Double
-            OpCodes.Nop, // 15 TypeCode.Decimal
-            OpCodes.Nop, // 16 TypeCode.DateTime
-            OpCodes.Nop, // 17 --
-            OpCodes.Nop // 18 TypeCode.String
-        };
-
-        static SQLite3Enumerator()
-        {
-            // 1. Get all object properties which can be set
-            var propertyInfos = new List<PropertyInfo>();
-            foreach (PropertyInfo property in typeof(T).GetProperties())
-                if (property.CanWrite)
-                    propertyInfos.Add(property);
-
-            // 2. Create the array with the object setters, and populate it
-            SetterMethods = new Dictionary<string, SetterMethod<T>>(StringComparer.InvariantCultureIgnoreCase);
-            foreach (PropertyInfo propertyInfo in propertyInfos)
-                SetterMethods.Add(propertyInfo.Name, GenerateSetterMethod(propertyInfo));
-        }
-
-        private static SetterMethod<T> GenerateSetterMethod(PropertyInfo propertyInfo)
-        {
-            var setter = new DynamicMethod(
-                "_set" + propertyInfo.Name,
-                null,
-                new[] { typeof(T), typeof(IntPtr), typeof(int) },
-                typeof(T),
-                true
-                );
-
-            ILGenerator generator = setter.GetILGenerator();
-            generator.Emit(OpCodes.Ldarg_0);
-            generator.Emit(OpCodes.Ldarg_1);
-            generator.Emit(OpCodes.Ldarg_2);
-            EmitColumnMethod(generator, propertyInfo.PropertyType);
-            EmitCastOpCode(generator, propertyInfo.PropertyType);
-            generator.Emit(OpCodes.Call, typeof(T).GetProperty(propertyInfo.Name).GetSetMethod());
-            generator.Emit(OpCodes.Ret);
-
-            return (SetterMethod<T>)setter.CreateDelegate(typeof(SetterMethod<T>));
-        }
-
-        private static void EmitColumnMethod(ILGenerator generator, Type type)
-        {
-            TypeCode typeCode = Type.GetTypeCode(type);
-
-            // If we have an object, check for an array
-            if (typeCode == TypeCode.Object)
-            {
-                if (type != typeof(byte[]))
-                    throw new SQLite3Exception("Invalid property type: " + type.Name);
-
-                generator.Emit(OpCodes.Call, ColumnGetters[(int)SQLite3ColumnType.Blob]);
-                return;
-            }
-
-            // TODO: Support for Decimal and DateTime and Nullable<T>
-            if (typeCode == TypeCode.Decimal || typeCode == TypeCode.DateTime)
-                throw new SQLite3Exception("Invalid property type: " + type.Name);
-
-            generator.Emit(
-                OpCodes.Call,
-                ColumnGetters[(int)SQLite3Convert.TypeCodeAffinities[(int)typeCode]]
-                );
-        }
-
-        private static void EmitCastOpCode(ILGenerator generator, Type type)
-        {
-            if (type == typeof(byte[]))
-            {
-                generator.Emit(OpCodes.Unbox_Any, typeof(byte[]));
-                return;
-            }
-
-            TypeCode typeCode = Type.GetTypeCode(type);
-            OpCode opCode = CastOpCodeAffinities[(int)typeCode];
-
-            if (opCode != OpCodes.Nop)
-                generator.Emit(opCode);
-        }
-
-        #endregion
-
-        protected readonly int ColumnCount;
-        protected readonly SetterMethod<T>[] Setters;
-        protected readonly SQLite3StatementHandle Statement;
-        protected readonly IntPtr StatementHandle;
+        protected readonly int _columnCount;
+        protected readonly IObjectHydrater<T> _hydrater;
+        protected readonly SQLite3StatementHandle _statement;
+        protected readonly SetterMethod<T>[] _setters;
 
         internal SQLite3Enumerator(SQLite3StatementHandle statement)
         {
-            Statement = statement;
-            StatementHandle = Statement;
+            _statement = statement;
+            _columnCount = NativeMethods.ColumnCount(_statement);
+            _setters = new SetterMethod<T>[_columnCount];
 
-            ColumnCount = NativeMethods.ColumnCount(Statement);
-            Setters = new SetterMethod<T>[ColumnCount];
+            var propertyNames = typeof(T).GetProperties().Where(p => p.CanWrite).Select(pi => pi.Name.ToLowerInvariant()).ToList();
 
-            for (int i = 0; i < ColumnCount; i++)
+            for (int i = 0; i < _columnCount; i++)
             {
-                string columnName = SQLite3Helper.ColumnName(Statement, i);
+                string columnName = SQLite3Helper.ColumnName(_statement, i).ToLowerInvariant();
+                if (!propertyNames.Contains(columnName))
+                    throw new SQLite3Exception("The object does not contain this property");
+            }
+
+            for (int i = 0; i < _columnCount; i++)
+            {
+                string columnName = SQLite3Helper.ColumnName(_statement, i);
                 if (!SetterMethods.ContainsKey(columnName))
                     throw new SQLite3Exception("The object does not contain this property");
 
-                Setters[i] = SetterMethods[columnName];
+                _setters[i] = SetterMethods[columnName];
             }
         }
-
-        #region Inherited Members
 
         #region IEnumerator<T> Members
 
@@ -158,8 +45,8 @@ namespace Interop.SQLite
             get
             {
                 var value = new T();
-                for (int i = 0; i < ColumnCount; i++)
-                    Setters[i](value, StatementHandle, i);
+                for (int i = 0; i < _columnCount; i++)
+                    _setters[i](value, _statementHandle, i);
 
                 return value;
             }
@@ -176,12 +63,12 @@ namespace Interop.SQLite
 
         public bool MoveNext()
         {
-            return SQLite3Helper.Step(Statement) == SQLite3Error.Row;
+            return SQLite3Helper.Step(_statement) == SQLite3Error.Row;
         }
 
         public void Reset()
         {
-            SQLite3Helper.Reset(Statement);
+            SQLite3Helper.Reset(_statement);
         }
 
         #endregion
@@ -190,10 +77,8 @@ namespace Interop.SQLite
 
         public void Dispose()
         {
-            Statement.Dispose();
+            _statement.Dispose();
         }
-
-        #endregion
 
         #endregion
     }
